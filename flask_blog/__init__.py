ad98f486c9ee6f91565a57741df30600d561d69a
Templars <Templars@TEMPLARS-PC>
2020-05-28 20:32:38 +0800

    Requirements PIP freeze

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -5,21 +5,30 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_blog.config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-from flask_blog.users.routes import users
-from flask_blog.posts.routes import posts
-from flask_blog.main.routes import main
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-app.register_blueprint(users)
-app.register_blueprint(posts)
-app.register_blueprint(main)
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+
+    from flask_blog.users.routes import users
+    from flask_blog.posts.routes import posts
+    from flask_blog.main.routes import main
+    from flask_blog.errors.handlers import errors
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+    app.register_blueprint(errors)
+    
+    return app
